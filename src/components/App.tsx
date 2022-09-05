76bd4238953d66ea666acd49b9e241d939296e2b
Diluen Cheong <diluen@outlook.com>
2022-09-05 22:56:03 +0900

    (Section 1 - Ep 9) - Custom Hooks

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef } from 'react';
 import '../reset.css';
 import '../App.css';
 import NoTodos from './NoTodos';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
+import useLocalStorage from '../hooks/useLocalStorage';
 
 function App() {
   interface Todo {
@@ -13,30 +14,32 @@ function App() {
     isEditing: boolean;
   }
 
-  const [name, setName] = useState('');
+  const [name, setName] = useLocalStorage('name', '');
   const nameInputEl = useRef<HTMLInputElement>(null);
-  const [todos, setTodos] = useState<Todo[]>([
-    {
-      id: 1,
-      title: 'Finish React Series',
-      isComplete: false,
-      isEditing: false,
-    },
-    {
-      id: 2,
-      title: 'Go Grocery',
-      isComplete: true,
-      isEditing: false,
-    },
-    {
-      id: 3,
-      title: 'Take over world',
-      isComplete: false,
-      isEditing: false,
-    },
-  ]);
+  const [todos, setTodos] = useLocalStorage('todos', []);
+  // const [todos, setTodos] = useState<Todo[]>([
+  //   {
+  //     id: 1,
+  //     title: 'Finish React Series',
+  //     isComplete: false,
+  //     isEditing: false,
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'Go Grocery',
+  //     isComplete: true,
+  //     isEditing: false,
+  //   },
+  //   {
+  //     id: 3,
+  //     title: 'Take over world',
+  //     isComplete: false,
+  //     isEditing: false,
+  //   },
+  // ]);
 
-  const [todoId, setTodoId] = useState(4);
+  // const [todoId, setTodoId] = useState(4);
+  const [todoId, setTodoId] = useLocalStorage('todoId', 1);
 
   function addTodo(todo: string) {
     setTodos([
@@ -49,7 +52,7 @@ function App() {
       },
     ]);
 
-    setTodoId(prevTodoId => prevTodoId + 1);
+    setTodoId((prevTodoId: number) => prevTodoId + 1);
   }
 
   function deleteTodo(id: Number) {
@@ -57,7 +60,7 @@ function App() {
   }
 
   function completeTodo(id: number) {
-    const updatedTodos = todos.map(todo => {
+    const updatedTodos = todos.map((todo: Todo) => {
       if (todo.id === id) {
         todo.isComplete = !todo.isComplete;
       }
@@ -69,7 +72,7 @@ function App() {
   }
 
   function markAsEditing(id: number) {
-    const updatedTodos = todos.map(todo => {
+    const updatedTodos = todos.map((todo: Todo) => {
       if (todo.id === id) {
         todo.isEditing = true;
       }
@@ -86,7 +89,7 @@ function App() {
       | React.KeyboardEvent<HTMLInputElement>,
     id: number
   ) {
-    const updatedTodos = todos.map(todo => {
+    const updatedTodos = todos.map((todo: Todo) => {
       if (todo.id === id) {
         if (event.currentTarget.value.trim().length === 0) {
           todo.isEditing = false;
@@ -104,7 +107,7 @@ function App() {
   }
 
   function cancelEdit(id: number) {
-    const updatedTodos = todos.map(todo => {
+    const updatedTodos = todos.map((todo: Todo) => {
       if (todo.id === id) {
         todo.isEditing = false;
       }
@@ -116,7 +119,7 @@ function App() {
   }
 
   function remainingCalculation() {
-    return todos.filter(todo => !todo.isComplete).length;
+    return todos.filter((todo: Todo) => !todo.isComplete).length;
   }
 
   const remaining = useMemo(remainingCalculation, [todos]);
@@ -126,7 +129,7 @@ function App() {
   }
 
   function completeAllTodos() {
-    const updatedTodos = todos.map(todo => {
+    const updatedTodos = todos.map((todo: Todo) => {
       todo.isComplete = true;
 
       return todo;
@@ -137,11 +140,11 @@ function App() {
 
   function todosFiltered(filter: string) {
     if (filter === 'active') {
-      return todos.filter(todo => !todo.isComplete);
+      return todos.filter((todo: Todo) => !todo.isComplete);
     }
 
     if (filter === 'completed') {
-      return todos.filter(todo => todo.isComplete);
+      return todos.filter((todo: Todo) => todo.isComplete);
     }
 
     return todos;
@@ -151,10 +154,17 @@ function App() {
     // console.log('use effect running');
     nameInputEl.current && nameInputEl.current.focus();
 
+    // setName(JSON.parse(localStorage.getItem('name') as string) ?? '');
+
     return function cleanup() {
       // console.log('cleaning up');
     };
   }, []);
+
+  function handleNameInput(event: React.ChangeEvent<HTMLInputElement>) {
+    setName(event.target.value);
+    // localStorage.setItem('name', JSON.stringify(event.target.value));
+  }
 
   return (
     <div className="todo-app-container">
@@ -168,7 +178,7 @@ function App() {
               className="todo-input"
               placeholder="What is your name"
               value={name}
-              onChange={event => setName(event.target.value)}
+              onChange={handleNameInput}
             />
           </form>
           {name && <p className="name-label">Hello, {name}</p>}
